a29dcf4830a7a6ad767b78fe5ed0d1a711693899
Lasha Abulashvili <labulashvili@gmail.com>
2019-03-22 15:42:15 +0400

    added awesome_print

--- template.rb
+++ template.rb
@@ -43,7 +43,7 @@ gem "sitemap_generator"
 gem "image_processing", "~> 1.2"
 
 gem_group :development, :test do
-  # Call "byebug" anywhere in the code to stop execution and get a debugger console
+  gem "awesome_print"
   gem "rspec-rails"
   gem "factory_bot_rails"
   gem "capybara"
