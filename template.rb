08b323bf43b54c035fe272f0d5eec4abe6da3f1b
Lasha Abulashvili <labulashvili@gmail.com>
2019-03-25 16:53:54 +0400

    modulirized templating steps into methods, now copying Procfile and example .env

--- template.rb
+++ template.rb
@@ -41,77 +41,100 @@ def nicely_format_gemfile
   inject_text_after("Gemfile", "# gem 'globalize', github: 'globalize/globalize'", "gem 'rails-i18n'")
   inject_text_after("Gemfile", " ", "gem 'image_processing', '~> 1.2'")
 end
+
+def add_gems
+  # General
+  gem 'sassc-rails'
+  gem 'aasm'
+  gem 'meta-tags'
+  gem 'http'
+  gem 'pagy'
+  gem 'slim-rails'
+  gem 'font-awesome-sass', '~> 5.6.1'
+  gem 'autoprefixer-rails'
+  gem 'paper_trail'
+
+  # Services
+  gem 'sendgrid-actionmailer'
+  gem 'aws-sdk-s3', '~> 1'
+  gem 'gravatar_image_tag'
+
+  # Security
+  gem 'devise'
+  gem 'omniauth-facebook'
+  gem 'omniauth-google-oauth2'
+  gem 'rolify'
+  gem 'pundit'
+  # gem 'devise_masquerade'
+  # gem 'devise-two-factor'
+  # gem 'devise_token_auth', "~> 0.2"
+  # gem 'recaptcha'
+
+  # Debugging & Optimization
+  gem 'active_record_query_trace'
+  gem 'rack-utf8_sanitizer'
+
+  # I18n
+  gem 'devise-i18n'
+  gem 'rails-i18n'
+  # gem 'globalize', github: 'globalize/globalize'
+
+  # Mechanical Line
+  gem 'redis', '~> 4.0'
+  gem 'hiredis'
+  gem 'sidekiq'
+  gem 'sidekiq-cron'
+  gem 'sidekiq-failures'
+  gem 'sitemap_generator'
+  gem 'image_processing', '~> 1.2'
+
+  gem_group :development, :test do
+    gem 'awesome_print'
+    gem 'rspec-rails'
+    gem 'factory_bot_rails'
+    gem 'capybara'
+    gem 'database_cleaner'
+    gem 'shoulda-matchers'
+    gem 'rails-controller-testing'
+  end
+
+  gem_group :development, :test, :staging do
+    gem 'faker', '1.9.3'
+  end
+
+  gem_group :development do
+    gem 'derailed_benchmarks'
+    gem 'rails-erd', require: false
+    gem 'i18n-tasks', '~> 0.9.6'
+    gem 'i18n_generators', '~> 2.1', '>= 2.1.1'
+  end
+end
+
+def remove_base_files
+  remove_file "app/assets/stylesheets/application.css"
+end
+
+def copy_base_files
+  base_path = File.join(Dir.home, "www", "template")
+  files_to_copy = %w[Procfile
+                     .env] # Procfile.dev
+  files_to_copy.each { |file| copy_file File.join(base_path, ".env"), file }
+end
+
 ############################ HELPER METHODS ############################
 ########################################################################
 
-# General
-gem 'sassc-rails'
-gem 'aasm'
-gem 'meta-tags'
-gem 'http'
-gem 'pagy'
-gem 'slim-rails'
-gem 'font-awesome-sass', '~> 5.6.1'
-gem 'autoprefixer-rails'
-gem 'paper_trail'
-
-# Services
-gem 'sendgrid-actionmailer'
-gem 'aws-sdk-s3', '~> 1'
-gem 'gravatar_image_tag'
-
-# Security
-gem 'devise'
-gem 'omniauth-facebook'
-gem 'omniauth-google-oauth2'
-gem 'rolify'
-gem 'pundit'
-# gem 'devise_masquerade'
-# gem 'devise-two-factor'
-# gem 'devise_token_auth', "~> 0.2"
-# gem 'recaptcha'
-
-# Debugging & Optimization
-gem 'active_record_query_trace'
-gem 'rack-utf8_sanitizer'
-
-# I18n
-gem 'devise-i18n'
-gem 'rails-i18n'
-# gem 'globalize', github: 'globalize/globalize'
-
-# Mechanical Line
-gem 'redis', '~> 4.0'
-gem 'hiredis'
-gem 'sidekiq'
-gem 'sidekiq-cron'
-gem 'sidekiq-failures'
-gem 'sitemap_generator'
-gem 'image_processing', '~> 1.2'
-
-gem_group :development, :test do
-  gem 'awesome_print'
-  gem 'rspec-rails'
-  gem 'factory_bot_rails'
-  gem 'capybara'
-  gem 'database_cleaner'
-  gem 'shoulda-matchers'
-  gem 'rails-controller-testing'
-end
-
-gem_group :development, :test, :staging do
-  gem 'faker', '1.9.3'
-end
-
-gem_group :development do
-  gem 'derailed_benchmarks'
-  gem 'rails-erd', require: false
-  gem 'i18n-tasks', '~> 0.9.6'
-  gem 'i18n_generators', '~> 2.1', '>= 2.1.1'
-end
+add_gems
 
 after_bundle do
   nicely_format_gemfile
+
+  # application name
+  environment "config.application_name = Rails.application.class.parent_name"
+
+  remove_base_files
+  copy_base_files
+
   git :init
   git add: '.'
   git commit: "-a -m 'Initial commit'"
