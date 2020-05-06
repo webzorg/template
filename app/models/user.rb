6ebaaf7f26d644b3930d4c18a89e97f73b3ba03b
Lasha Abulashvili <labulashvili@gmail.com>
2020-05-07 03:23:38 +0400

    add missing notifications migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < Lasha::User
+  has_many :notifications, dependent: :destroy
+
+  def web_notifications
+    # # Notification.create(
+    # #   notify_type: "",
+    # #   actor: user,
+    # #   user: target_user,
+    # #   target: target_object
+    # # )
+    # notifications.where(notify_type: %i[]).order(created_at: :desc)
+    notifications.order(created_at: :desc)
+  end
 end
