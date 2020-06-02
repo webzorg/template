af773d477b5f167be4e0b409cbc970f98dc94f2a
Lasha Abulashvili <labulashvili@gmail.com>
2020-06-02 23:37:58 +0400

    remove engine - dummy app

--- dummy_app/spec/dummy/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # devise_for :users
-  resources :trees
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "trees#index"
-end
--- dummy_app/spec/dummy/db/migrate/20190912205616_create_gardens.rb
+++
@@ -1,9 +0,0 @@
-class CreateGardens < ActiveRecord::Migration[5.2]
-  def change
-    create_table :gardens do |t|
-      t.text :name
-
-      t.timestamps
-    end
-  end
-end
